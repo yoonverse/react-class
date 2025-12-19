6be5e559ab4beb95653600d7c06c5127d976eb6a
yoonverse <the.yoonverse@gmail.com>
2025-12-20 08:49:42 +0900

    과제 01 - 완료

--- homework/index.js
+++ homework/index.js
@@ -22,6 +22,56 @@ const 게시글등록페이지 = () => {
                 </div>
             </div>
             <div className="divider" />
+            <div className="form-row">
+                <div className="form-group">
+                    <label className="label">내용<span className="required">*</span></label>
+                    <textarea type="textarea" className="textarea" placeholder="내용을 입력해 주세요."></textarea>
+                </div>
+            </div>
+            <div clasName="form-row">
+                <div className="form-group">
+                    <label className="label">주소</label>
+                    <div className="zipcode-row">
+                        <input type="text" className="input zipcode" placeholder="01234" readonly/>
+                        <button input="button" className="btn outline">우편번호 검색</button>
+                    </div>
+                    <input type="text" className="input" placeholder="주소를 입력해 주세요." readonly/>
+                    <input type="text" className="input" placeholder="상세주소"></input>
+                </div>
+            </div>
+            <div className="divider" />
+            <div className="form-row">
+                <div className="form-group">
+                    <label className="label">유튜브 링크</label>
+                    <input type="url" className="input" placeholder="링크를 입력해 주세요." />
+                </div>
+            </div>
+            <div className="divider" />
+            <div className="form-row">
+                <div className="form-group">
+                    <label className="label">사진 첨부</label>
+                    <div className="img-upload-group">
+                        <div className="img-upload">
+                            <span className="icon"></span>
+                            <span className="text">클릭해서 사진 업로드</span>
+                        </div>
+                        <div className="img-upload">
+                            <span className="icon"></span>
+                            <span className="text">클릭해서 사진 업로드</span>
+                        </div>
+                        <div className="img-upload">
+                            <span className="icon"></span>
+                            <span className="text">클릭해서 사진 업로드</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="button-row">
+                <div className="button-group">
+                    <button type="button" className="btn outline">취소</button>
+                    <button type="submit" className="btn fill">등록하기</button>
+                </div>
+            </div>
         </form> 
     )
 }
